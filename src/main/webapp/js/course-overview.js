3907ec07ea8a03418f621f931e112bfba67c3ba5
ThomasVanOpstal <91283935+ThomasVanOpstal@users.noreply.github.com>
2022-11-14 15:42:52 +0100

    Initial commit

--- src/main/webapp/js/course-overview.js
+++ src/main/webapp/js/course-overview.js
@@ -4,8 +4,8 @@ const renderCourse = () => {
         course.forEach((course) => {
                 const p = document.createElement("p")
                 const text = document.createTextNode(`${course.name} 
-                   is een ${course.type.toLocaleLowerCase()} van 
-                   ${course.credits} credits.`)
+                   is een course gegeven door ${course.lectors.join(" en ")}met 
+                   ${course.credits} studiepunten .`)
                 p.appendChild(text)
                 document.getElementById(id).appendChild(p)
         })
--- src/main/webapp/js/course.js
+++ src/main/webapp/js/course.js
@@ -1,5 +1,4 @@
 const course = [
     {"name": "Web3", "lectors": ["Greetje Jongen","Elke Steegmans"], credits: 8},
     {"name":"Software Testing", "lectors": ["Johan Pieck", "David Vandenbroeck"], credits:5},
-    {"name": "Web4", "lectors": ["Greetje Jongen","Elke Steegmans"], credits: 6}
 ]
