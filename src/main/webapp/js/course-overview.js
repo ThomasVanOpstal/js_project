30800b9137f88fb7b5cdfdac0b6da0a1e01bd3bd
BaeyensRobbe <robbe.baeyens@student.ucll.be>
2022-11-14 15:45:20 +0100

    course

--- src/main/webapp/js/course-overview.js
+++ src/main/webapp/js/course-overview.js
@@ -4,7 +4,7 @@ const renderCourse = () => {
         course.forEach((course) => {
                 const p = document.createElement("p")
                 const text = document.createTextNode(`${course.name} 
-                   is een ${course.type.toLocaleLowerCase()} van 
+                   is een course gegeven door ${course.lectors.join(" en ")} van 
                    ${course.credits} credits.`)
                 p.appendChild(text)
                 document.getElementById(id).appendChild(p)
