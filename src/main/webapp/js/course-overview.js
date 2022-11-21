0026e81297d8e9e7425f7d0efce5b0c0e880c97c
BaeyensRobbe <robbe.baeyens@student.ucll.be>
2022-11-21 14:53:44 +0100

    Merge branch 'main' of https://github.com/ThomasVanOpstal/js_project

--- src/main/webapp/js/course-overview.js
+++ src/main/webapp/js/course-overview.js
@@ -4,8 +4,7 @@ const renderCourse = () => {
         course.forEach((course) => {
                 const p = document.createElement("p")
                 const text = document.createTextNode(`${course.name} 
-                   is een course gegeven door ${course.lectors.join(" en ")} van 
-                   ${course.credits} credits.`)
+                   is een course gegeven door ${course.lectors.join(" en ")}`)
                 p.appendChild(text)
                 document.getElementById(id).appendChild(p)
         })
--- src/main/webapp/js/course.js
+++ src/main/webapp/js/course.js
@@ -1,4 +1,5 @@
 const course = [
-    {"name": "Web3", "lectors": ["Greetje Jongen","Elke Steegmans"], credits: 8},
-    {"name":"Software Testing", "lectors": ["Johan Pieck", "David Vandenbroeck"], credits:5},
+    {"name": "Web3", "lectors": ["Greetje Jongen","Elke Steegmans"], credits: 8, semester: 1},
+    {"name":"Software Testing", "lectors": ["Johan Pieck", "David Vandenbroeck"], credits:5, semester: 1},
+    {"name":"Web4", "lectors": ["Greetje Jongen", "Johan Pieck"], credits:6, semester: 2}
 ]
